dc9daa44ee3d3119acab59e195a1b138e884c9cf
amulya-vijay <amulyakvijay297@gmail.com>
2021-06-01 15:52:13 +0530

    Maximum of three float values with three test cases

--- MaximumValue.java
+++ MaximumValue.java
@@ -2,7 +2,7 @@ package com.maxValue;
 
 public class MaximumValue{
 	
-	public Integer getMaxValue(Integer a,Integer b, Integer c) {
+	public Integer getMaxInteger(Integer a,Integer b, Integer c) {
 		
 		Integer maximum = a;
 		if(b.compareTo(maximum)>0) {
@@ -11,8 +11,19 @@ public class MaximumValue{
 		if(c.compareTo(maximum)>0) {
 			maximum = c;
 		}
-		return maximum;
+		return maximum;		
+	}
+	
+public Float getMaxFloat(Float f1,Float f2, Float f3) {
 		
+		Float maximum = f1;
+		if(f2.compareTo(maximum)>0) {
+			maximum = f2;
+		}
+		if(f3.compareTo(maximum)>0) {
+			maximum = f3;
+		}
+		return maximum;		
 	}
 
 }
